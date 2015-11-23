b3e0b294480af0458697519da1a82244b3eb67a4
Somnuk Wongkhan <somnuk.wk@gmail.com>
2015-11-24 01:14:01 +0700

    test flatten

--- 4clojure.clj
+++ 4clojure.clj
@@ -52,3 +52,55 @@
 
 (= (list 6 7) (filter #(> % 5) '(3 4 5 6 7)))
 
+(= [20 30 40] (rest [10 20 30 40]))
+
+(= 8 ((fn add-five [x] (+ x 5)) 3))
+(= 8 ((fn [x](+ x 5)) 3))
+(= 8 (#(+ % 5) 3))
+(= 8 ((partial + 5) 3))
+
+
+;; 19
+(= (last [1 2 3 4 5]) 5)
+(= (last '(5 4 3)) 3)
+(= (last ["b" "c" "d"]) "d")
+
+;; 20
+
+(= (#(second (reverse %)) (list 1 2 3 4 5)) 4)
+(= 1 1)
+
+(= (nth '(4 5 6 7) 2) 6)
+(= (nth [:a :b :c] 0) :a)
+
+(= (count '(1 2 3 3 1)) 5)
+(= (count "Hello world") 11)
+(= (count [[1 2] [2 4] [5 6]]) 3)
+
+(= (reverse [1 2 3 4 5]) [5 4 3 2 1])
+
+(= (reverse (sorted-set 5 7 2 7)) '(7 5 2))
+
+
+;; sum
+(= (reduce + [1 2 3]) 6)
+
+
+(= (filter odd? #{1 2 3 4 5}) '(1 3 5))
+
+(def fib-seq
+  (lazy-cat [0 1]
+            (map + fib-seq(rest fib-seq))))
+
+(take 10 fib-seq)
+
+(false? (#(= (reverse %) (seq %)) '(1 2 3 4 5)))
+
+
+(= (flatten '((1 2) 3 [4 [5 6]])) '(1 2 3 4 5 6))
+
+
+;;29
+;;(= (#(apply str (re-seq #"[A-Z]" %) "HeLl0, WoRlD!") "HLOWRD")
+
+
